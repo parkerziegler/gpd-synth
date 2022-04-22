f2cc2b6895d2cd8f7cd71f2285683289c3e8d804
Logan Caraco <lbc.dna@gmail.com>
2022-04-22 15:28:06 -0700

    dataclass optimization

--- src/grammar.py
+++ src/grammar.py
@@ -1,3 +1,6 @@
+from typing import Protocol
+from dataclasses import dataclass
+
 import geopandas as gpd
 
 # The grammar for our synthesis engine.
@@ -5,37 +8,55 @@ import geopandas as gpd
 # Each part of the grammar is represented as a separate class with __init__, __repr__, and interpret methods.
 # __repr__ provides the string representation to use to display the synthesized program.
 # interpret provides the actual code to execute behind the scenes when evaluating the synthesized program.
-class GDF:
-    def __init__(self, name):
-        self.name = name
+
+class GrammarRule(Protocol):
+    def interpret(self, gdfs: dict[str, 'GrammarRule']) -> gpd.GeoDataFrame:
+        raise NotImplementedError('Not Yet Implemented')
+    
+    def __repr__(self) -> str:
+        raise NotImplementedError('Not Yet Implemented')
+
+
+# Optimization Note: On the use of @dataclass
+#   Dataclasses are about 5x faster to instantiate than
+#   standard classes, but about 1.25x slower to read attributes from.
+#   
+#   For our purposes, we are generating many, many instances, and accessing
+#   each field once. Also, instantiation in any layout takes far longer than
+#   reading attributes from any layout. 
+
+
+@dataclass(frozen=True, repr=False)
+class GDF(GrammarRule):
+    name: str
 
     def __repr__(self):
-        return f"{self.name}"
+        return self.name
 
     def interpret(self, gdfs):
         return gdfs[self.name]
 
 
-class Dissolve:
-    def __init__(self, gdf, by):
-        self.gdf = gdf
-        self.by = by
+@dataclass(frozen=True, repr=False)
+class Dissolve(GrammarRule):
+    gdf: GDF
+    by: str
 
     def __repr__(self):
-        return str(self.gdf) + ".dissolve(by=" + f'"{self.by}"' + ")"
+        return f'{self.gdf}.dissolve(by="{self.by}")'
 
     def interpret(self, gdfs):
         gdf = self.gdf.interpret(gdfs)
         return gdf.dissolve(by=self.by)
 
 
-class SJoin:
-    def __init__(self, left, right):
-        self.left = left
-        self.right = right
+@dataclass(frozen=True, repr=False)
+class SJoin(GrammarRule):
+    left: str
+    right: str
 
     def __repr__(self):
-        return "gpd.sjoin(" + str(self.left) + ", " + str(self.right) + ")"
+        return f"gpd.sjoin({self.left}, {self.right})"
 
     def interpret(self, gdfs):
         return gpd.sjoin(gdfs[self.left], gdfs[self.right])
