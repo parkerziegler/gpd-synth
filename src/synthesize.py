d722b16de7cc354e478010845fd10fb02cdf9a4e
Logan Caraco <lbc.dna@gmail.com>
2022-04-25 15:32:52 -0700

    made basic pieces

--- src/synthesize.py
+++ src/synthesize.py
@@ -89,16 +89,16 @@ def synthesize(gdfs, target):
 
 from pandas import DataFrame
 from geopandas import GeoDataFrame
-from typing import Callable, Generator
-from grammar import GrammarRule, Merge, SJoin
-from collections import defaultdict
+from typing import Callable, Generator, TypeAlias
 from itertools import product, combinations
 
-GdfBindings = dict[str, DataFrame]
-CandidateGen = Generator[GrammarRule, None, None]
-LazySynthesizer = Callable[[GdfBindings], CandidateGen]
+from grammar import GrammarRule, Merge, SJoin
+from synth_input import GdfBindings
 
-def lazy_univariate(gdfs: GdfBindings) -> CandidateGen:
+CandidateGen:    TypeAlias = Generator[GrammarRule, None, None]
+LazySynthesizer: TypeAlias = Callable[[GdfBindings], CandidateGen]
+
+def univariate(gdfs: GdfBindings) -> CandidateGen:
     'Generates the simplest programs first, including equivalents.'
     for gdf_name in gdfs.keys():
         yield GDF(gdf_name)
@@ -109,66 +109,56 @@ def lazy_univariate(gdfs: GdfBindings) -> CandidateGen:
                 yield Dissolve(GDF(gdf_name), col)
 
 
+def bivariate(gdfs: GdfBindings) -> CandidateGen:
+    merge_gen = merge(gdfs)
+    sjoin_gen = sjoin(gdfs)
+    for m_candidate, s_candidate in zip(merge_gen, sjoin_gen):
+        yield m_candidate
+        yield s_candidate
+    yield from sjoin_gen
+    yield from merge_gen
+
+
 def make_candidate_filter(gdfs: GdfBindings, target: DataFrame):
     'Returns a predicate that checks if a `program` over `gdfs` matches `target`'
     def candidate_filter(program: DataFrame) -> bool:
         return gdfs_equal(evaluate_program(program, gdfs), target)
     return candidate_filter
 
-def lazy_synth(gdfs: GdfBindings, synthesizer: LazySynthesizer, target: DataFrame):
+
+def lazy_synth(gdfs: GdfBindings, synthesizer: LazySynthesizer, target: DataFrame) -> CandidateGen:
     'An iterator over all programs over `gdfs` that match `target`'
     checker = make_candidate_filter(gdfs, target)
     return filter(checker, synthesizer(gdfs))
 
-def lazy_synthesize(gdfs: GdfBindings, target: DataFrame):
+
+def lazy_synthesize(gdfs: GdfBindings, target: DataFrame) -> None:
     'Lazy counterpart to `synthesize`'
-    print(next(lazy_synth(gdfs, lazy_univariate, target), 'No program found!'))
-
-
-def cols_by_dtype(frame: DataFrame) -> dict[type, set[str]]:
-    out = defaultdict(set)
-    for k, v in dict(frame.dtypes).items():
-        out[v].add(k)
-    return dict(out)
+    print(next(lazy_synth(gdfs, univariate, target), 'No program found!'))
 
 
 def binding_pairs(gdfs: GdfBindings):
     yield from combinations(gdfs.items(), 2)
 
 
-def merge(gdfs: GdfBindings) -> CandidateGen:
-    for (l, l_frame), (r, r_frame) in binding_pairs(gdfs):
-        l_types = cols_by_dtype(l_frame)
-        r_types = cols_by_dtype(r_frame)
+def merge(gdfs: GdfBindings):
+    for (l_name, _), (r_name, _) in binding_pairs(gdfs):
+        l_types = gdfs.get_cols_dtype(l_name)
+        r_types = gdfs.get_cols_dtype(r_name)
         for k, l_v in l_types.items():
             if r_v := r_types.get(k, None):
                 for l_col, r_col in product(l_v, r_v):
                     # 'cross' throws weird exception
                     for h in ("left", "right", "inner", "outer"):  
-                        yield Merge(l, r, how=h, left_on=l_col, right_on=r_col)
+                        yield Merge(l_name, r_name, how=h, \
+                            left_on=l_col, right_on=r_col)
 
 
-def sjoin(gdfs: GdfBindings) -> CandidateGen:
+def sjoin(gdfs: GdfBindings):
     for (l, l_frame), (r, r_frame) in binding_pairs(gdfs):
-        if not isinstance(l_frame, GeoDataFrame) or \
-            not isinstance(r_frame, GeoDataFrame):
-                continue
-        query_preds = l_frame.sindex.valid_query_predicates & \
-            r_frame.sindex.valid_query_predicates
-        for h in ("left", "right", "inner"):
-            for p in query_preds:
-                yield SJoin(l, r, how=h, predicate=p)
-
-
-def binop(gdfs: GdfBindings) -> CandidateGen:
-    merge_gen = merge(gdfs)
-    sjoin_gen = sjoin(gdfs)
-    for m_candidate, s_candidate in zip(merge_gen, sjoin_gen):
-        yield m_candidate
-        yield s_candidate
-    test = next(merge_gen, None)
-    if test is None:
-        yield from sjoin_gen
-    else:
-        yield test
-        yield from merge_gen
+        if type(l_frame) == GeoDataFrame and type(r_frame) == GeoDataFrame:
+            query_preds = l_frame.sindex.valid_query_predicates & \
+                r_frame.sindex.valid_query_predicates
+            for h in ("left", "right", "inner"):
+                for p in query_preds:
+                    yield SJoin(l, r, how=h, predicate=p)
