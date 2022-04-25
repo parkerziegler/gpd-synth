2ae04e9c0fad26e73d440715717a025c79d555a0
Logan Caraco <lbc.dna@gmail.com>
2022-04-25 16:01:22 -0700

    cut unused defs

--- src/synthesize.py
+++ src/synthesize.py
@@ -11,88 +11,6 @@ CandidateGen:    TypeAlias = Generator[GrammarRule, None, None]
 LazySynthesizer: TypeAlias = Callable[[GdfBindings], CandidateGen]
 
 
-# grow applies every non-terminal in the grammar to every possible combination of
-# subexpressions drawn from program_list.
-def grow(program_list):
-    expansion = []
-    # Our grammar needs to have nonterminals in order for this to be relevant!
-
-    return expansion
-
-
-# Evaluate a candidate expression given the dictionary of input GeoDataFrames.
-# This is the core function responsible for running a synthesized program on our inputs.
-def evaluate_program(program, input_gdfs):
-    return program.interpret(input_gdfs)
-
-
-# Check whether two GeoDataFrames are equivalent.
-def gdfs_equal(left, right):
-    # Optimization Note:
-    #   Using assert_geodataframe_equal adds substantial penalty for 
-    #   constructing and using a try/except block
-    return GeoDataFrame.equals(left, right)
-
-
-# Check whether a candidate program is observationally equivalent to an already
-# synthesized program.
-def already_accounted_for(program_list, program, input_gdfs):
-    output = evaluate_program(program, input_gdfs)
-
-    for existing_program in program_list:
-        if gdfs_equal(output, evaluate_program(existing_program, input_gdfs)):
-            return True
-
-    return False
-
-
-# Eliminate all programs that already have an observational equivalent in our candidate program_list.
-def elim_equivalents(program_list, input_gdfs):
-    pruned_list = []
-
-    for program in program_list:
-        if already_accounted_for(pruned_list, program, input_gdfs):
-            continue
-
-        pruned_list.append(program)
-
-    return pruned_list
-
-
-# The core synthesis function. We use enumerative synthesis with observational equivalence pruning.
-#
-# In this algorithm, we're recursively building up larger and larger programs
-# based on our grammar, eargely pruning those that behave identically on all inputs.
-def synthesize(gdfs, target):
-    program_list = []
-
-    for gdf_name in gdfs.keys():
-        program_list.append(GDF(gdf_name))
-
-    for gdf_name, gdf in gdfs.items():
-        for col in gdf.columns:
-            if col != "geometry":
-                program_list.append(Dissolve(gdf_name, col))
-
-    # Binary operations here.
-
-    print(
-        "Candidates\n",
-        "\n".join([str(program) for program in program_list]),
-    )
-
-    program_list = elim_equivalents(program_list, gdfs)
-
-    # grow will go here if we end up having non-terminals.
-
-    for program in program_list:
-        if gdfs_equal(evaluate_program(program, gdfs), target):
-            print("\nFound it!", program)
-            return program
-
-    print("No program found!")
-
-
 def program(gdfs: GdfBindings) -> CandidateGen:
     yield from univariate(gdfs)
     yield from bivariate(gdfs)
@@ -119,11 +37,9 @@ def bivariate(gdfs: GdfBindings) -> CandidateGen:
     yield from merge_gen
 
 
-def make_candidate_filter(gdfs: GdfBindings, target: DataFrame):
+def make_candidate_filter(gdfs: GdfBindings, target: DataFrame) -> Callable[[GrammarRule], bool]:
     'Returns a predicate that checks if a `program` over `gdfs` matches `target`'
-    def candidate_filter(program: DataFrame) -> bool:
-        return gdfs_equal(evaluate_program(program, gdfs), target)
-    return candidate_filter
+    return lambda program: GeoDataFrame.equals(program.interpret(gdfs), target)
 
 
 def lazy_synth(gdfs: GdfBindings, synthesizer: LazySynthesizer, target: DataFrame) -> CandidateGen:
@@ -138,10 +54,12 @@ def lazy_synthesize(gdfs: GdfBindings, target: DataFrame) -> None:
 
 
 def binding_pairs(gdfs: GdfBindings):
-    yield from combinations(gdfs.items(), 2)
+    'Returns a generator over all unique (unordered) pairs of DataFrames in gdfs'
+    return combinations(gdfs.items(), 2)
 
 
 def merge(gdfs: GdfBindings):
+    'Generates all valid pd.merge calls over gdfs'
     for (l_name, _), (r_name, _) in binding_pairs(gdfs):
         l_types = gdfs.get_cols_dtype(l_name)
         r_types = gdfs.get_cols_dtype(r_name)
@@ -155,6 +73,7 @@ def merge(gdfs: GdfBindings):
 
 
 def sjoin(gdfs: GdfBindings):
+    'Generates all valid gpd.sjoin calls over gdfs'
     for (l, l_frame), (r, r_frame) in binding_pairs(gdfs):
         if type(l_frame) == GeoDataFrame and type(r_frame) == GeoDataFrame:
             query_preds = l_frame.sindex.valid_query_predicates & \
